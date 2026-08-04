0add9fbe6c79f131b857564deb55ed8304f7ad17
Andrés Joshua León Barranco <153522347+joshualeba@users.noreply.github.com>
2026-08-04 09:00:29 -0600

    Agregados límites de memoria para logs

--- mercy_app/src/components/FullScreenLoader.js
+++ mercy_app/src/components/FullScreenLoader.js
@@ -9,13 +9,14 @@ const HTML_LOADER = `
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 <style>
-  body {
+  html, body {
     margin: 0;
     padding: 0;
+    width: 100%;
+    height: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 100vh;
     background-color: transparent;
   }
   .loader {
